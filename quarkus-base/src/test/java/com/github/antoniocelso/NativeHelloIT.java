b478569419465bac50c1feb358ade88d4625b147
C1314151 Antonio Celso Prado De Magalhaes <c1314151@interno.bb.com.br>
2020-07-10 14:43:44 -0300

    Atualização de Projeto Base Quarkus

---
+++ quarkus-base/src/test/java/com/github/antoniocelso/NativeHelloIT.java
@@ -0,0 +1,9 @@
+package com.github.antoniocelso;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloIT extends HelloTest {
+
+    // Execute the same tests but in native mode.
+}
