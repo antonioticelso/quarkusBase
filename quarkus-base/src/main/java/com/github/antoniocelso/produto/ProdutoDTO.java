14677cb4a23dee5e35fa17fe6b1ef91ce1476bcd
Antonio Celso <antonio.ti.celso@gmail.com>
2020-08-01 01:27:34 -0300

    Atualizacao de projeto com CRUD funcionando via Swagger

---
+++ quarkus-base/src/main/java/com/github/antoniocelso/produto/ProdutoDTO.java
@@ -0,0 +1,11 @@
+package com.github.antoniocelso.produto;
+
+import java.math.BigDecimal;
+
+public class ProdutoDTO {
+
+    public String nome;
+
+    public BigDecimal valor;
+
+}
--- quarkus-base/src/main/java/com/github/antoniocelso/produto/ProdutoResource.java
+++ quarkus-base/src/main/java/com/github/antoniocelso/produto/ProdutoResource.java
@@ -1,8 +1,10 @@
 package com.github.antoniocelso.produto;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.Optional;
 
 @Path("/v1/produtos")
 @Produces(MediaType.APPLICATION_JSON)
@@ -14,8 +16,35 @@ public class ProdutoResource {
         return Produto.listAll();
     }
 
-//    @POST
-//    public void listarProdutos(Cadas) {
-//        Produto.listAll();
-//    }
+    @POST
+    @Transactional
+    public void listarProdutos(ProdutoDTO dto) {
+
+        Produto produto = new Produto();
+
+        produto.nome = dto.nome;
+        produto.valor = dto.valor;
+        produto.persist();
+
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void atualizarProdutos(@PathParam("id") Long id, ProdutoDTO dto) {
+
+        Optional<Produto> produtoOptional = Produto.findByIdOptional(id);
+
+        if ( produtoOptional.isPresent() ) {
+            Produto produto = produtoOptional.get();
+            produto.nome = dto.nome;
+            produto.valor = dto.valor;
+            produto.persist();
+
+        } else {
+            throw new NotFoundException();
+        }
+
+    }
+
 }
